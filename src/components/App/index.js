616387db65668f87ab705b3b28175d5b2b3534f9
Lucas-Millet <lucamillet@gmail.com>
2021-10-03 14:29:50 +0200

    form de base + champs controlés

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,17 +1,49 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
+
 
 // == Import
-import reactLogo from './react-logo.svg';
+
 import './styles.css';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-  </div>
-);
+const App = () => 
+{ 
+  const [state, setState] = React.useState({
+    firstname: "",
+    lastname:"",
+    email:"",
+    phone:"",
+  })
+  
+  const handleChange = (e) => {
+    setState({ ...state,
+      [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="app">
+
+      <form id="survey" onSubmit={handleSubmit}>
+        <label>Prénom</label>
+        <input type="text" name="firstname" value={state.firstname} onChange={handleChange}/>
+        <label>Nom</label>
+        <input type="text" name="lastname" value={state.lastname} onChange={handleChange}/>
+        <label>Email</label>
+        <input type="email" name="email" value={state.email} onChange={handleChange}/>
+        <label>Télephone</label>
+        <input type="text" name="phone" value={state.phone} onChange={handleChange}/>
+       
+        <input type="submit"/>
+      </form>
+
+    </div>
+  );
+}
 
 // == Export
 export default App;
